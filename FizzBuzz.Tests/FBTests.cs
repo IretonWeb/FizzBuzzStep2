6954adfba0725235b3401a7b9145fe0371c66087
IretonWeb <info@iretonweb.co.uk>
2013-11-12 12:40:45 +0000

    Fizz Buzz Step 2

---
+++ FizzBuzz.Tests/FBTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    class FBTests
+    {
+
+
+        [Test]
+        public void MulitpleNumber3Test()
+        {
+
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("fizz", FizzBuzz.WorkOut(6));
+        }
+        [Test]
+        public void Number3Test()
+        {
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("lucky", FizzBuzz.WorkOut(13));
+        }
+        [Test]
+        public void Number5Test()
+        {
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("buzz", FizzBuzz.WorkOut(5));
+        }
+
+        [Test]
+        public void Number15Test()
+        {
+
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("fizzbuzz", FizzBuzz.WorkOut(15));
+        }
+
+        [Test]
+        public void DifferentNumberTest()
+        {
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase("1", FizzBuzz.WorkOut(1));
+            
+        }
+       
+       
+
+    }
+
+    
+}
